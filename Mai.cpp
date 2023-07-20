5bc7751ca250aa5c947a6be0844004deef1c45e0
maisoravit <135790230+maisoravit@users.noreply.github.com>
2023-07-20 17:39:32 +0700

    Add files via upload

--- Mai.cpp
+++ Mai.cpp
@@ -1,9 +1,40 @@
+// #include <iostream>
+
+// int main() {
+
+//   std::cout << "1\n";
+//   std::cout << "2\n";
+//   std::cout << "3\n";
+
+// }
+
 #include <iostream>
+using namespace std;
 
 int main() {
 
-  std::cout << "1\n";
-  std::cout << "2\n";
-  std::cout << "3\n";
+  int year;
 
+  cout << "Enter a year: ";
+  cin >> year;
+  int sum = year-543;
+  cout << sum << "\n";
+if (sum % 4 == 0) {
+    if (sum % 100 == 0) {
+      if (sum % 400 == 0) {
+        cout << sum << " is a leap year.";
+      }
+      else {
+        cout << sum << " is not a leap year.";
+      }
+    }
+    else {
+      cout << sum << " is a leap year.";
+    }
+  }
+  else {
+    cout << sum << " is not a leap year.";
+  }
+
+  return 0;
 }
